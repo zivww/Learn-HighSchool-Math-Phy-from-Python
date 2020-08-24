cb6af8db72a8e2b9aaacee38c35b8612a7f38e23
zivww <ziv.wang.tw@gmail.com>
2020-08-24 21:49:25 +0800

    [Python] d1-d2

---
+++ day_1_preparing.py
@@ -0,0 +1,21 @@
+# Install Python 3
+Windows:
+Python Website https://www.python.org/downloads/windows/ 
+
+Click:
+Download Windows x86-64 executable installer
+
+# Install Good Terminal: Cmder
+https://cmder.net/
+
+# Use Terminal to Test:
+python --version
+
+# Install Jupyter Notebook
+https://jupyter.org/install
+
+python -m pip install --upgrade pip
+python -m pip install jupyter
+
+# Start the jupyter
+jupyter notebook
---
+++ day_3_loops.py
@@ -0,0 +1,31 @@
+# Symbols and Math
+
+## Math
+
+10/2
+
+10%3
+
+10*2
+
+10**2
+
+10//3
+
+## How to transfer decimal to binary ?
+
+bin(8)
+
+
+## How to transfer decimal to hex ?
+
+hex(10)
+
+
+## How to transfer binary to decimal ?
+
+int('0b1000', 2)
+
+## How to transfer binary to hex ?
+
+int('0xa', 16)
